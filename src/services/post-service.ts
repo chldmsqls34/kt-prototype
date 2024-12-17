d898b6acd8d2a85a74d2a0549b8538fca8005d64
chldmsqls34 <chldmsqls34@gmail.com>
2024-12-17 15:53:05 +0900

    refactor: 게시물 작성일 날짜 포맷팅

--- src/services/post-service.ts
+++ src/services/post-service.ts
@@ -62,7 +62,13 @@ export async function fetchFilteredPost(
       userId: post.user_id,
       title: post.title,
       content: post.content,
-      createdAt: new Date(post.created_at).toISOString(),
+      createdAt: new Date(post.created_at).toLocaleDateString('ko-KR', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+      }),
       viewCount: post.view_count,
       author: post.author,
     }));
