701e3cad5be0e2e4260d1cf0602c4c4c47309d3a
chldmsqls34 <chldmsqls34@gmail.com>
2024-12-17 15:46:29 +0900

    feat: auth route 추가 및 mypage 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import SignoutButton from "@/components/common/signout-button";
+
 export default function Page() {
-  return <div className="text-[--blue-color-100]">dfd</div>
+  return (
+    <div className="text-[--blue-color-100]">
+      <SignoutButton/>
+    </div>
+  )
 }
