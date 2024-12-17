3db1098c3bd6f5fe76c059b18d1212e05bbc1de7
chldmsqls34 <chldmsqls34@gmail.com>
2024-12-17 15:44:02 +0900

    feat: supabase auth 회원가입 및 로그인 기능 추가

---
+++ src/app/login/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { login, signup } from "@/services/auth-action";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+
+export default function LoginPage() {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const togglePassword = () => setShowPassword((prevState) => !prevState);
+  
+  return (
+    <form>
+      <Card className="w-[500px] p-5">
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-2xl">로그인</CardTitle>
+          <CardDescription>로그인을 위한 정보를 입력해주세요.</CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-6">
+          <div className="grid gap-2">
+            <label htmlFor="email">이메일</label>
+            <input id="email" name="email" type="email" placeholder="이메일을 입력하세요." required />
+
+          </div>
+          <div className="relative grid gap-2">
+            <div className="flex items-center">
+              <label htmlFor="password">비밀번호</label>
+            </div>
+            <input id="password" name="password" type={showPassword ? "text" : "password"} placeholder="비밀번호를 입력하세요." required />
+            <button className="absolute top-[38px] right-2 -translate-y-1/4 bg-transparent hover:bg-transparent" onClick={togglePassword}>
+              {showPassword ? <EyeSlashIcon className="h-5 w-5 text-muted-foreground" /> : <EyeIcon className="h-5 w-5 text-muted-foreground" />}
+            </button>
+          </div>
+        </CardContent>
+        <div className="relative">
+          <div className="absolute inset-0 flex items-center">
+            <span className="w-full border-t"></span>
+          </div>
+          <div className="relative flex justify-center text-xs uppercase">
+          </div>
+        </div>
+        <CardFooter className="flex mt-6 gap-4">
+          <button 
+            className="px-10 py-2 text-sm text-white bg-[#E79057] hover:bg-[#E26F24] hover:ring-1 hover:ring-[#E26F24] hover:ring-offset-1 active:bg-[#D5753D] hover:shadow-lg" 
+            formAction={login}
+          >
+            로그인
+          </button>
+          <button 
+            className="px-10 py-2 text-sm text-white bg-[#E79057] hover:bg-[#E26F24] hover:ring-1 hover:ring-[#E26F24] hover:ring-offset-1 active:bg-[#D5753D] hover:shadow-lg" 
+            formAction={signup}
+          >
+            회원가입
+          </button>
+        </CardFooter>
+      </Card>
+    </form>
+  )
+}
---
+++ src/services/auth-action.ts
@@ -0,0 +1,66 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+import { createClient } from '@/utils/supabase/server'
+
+export async function login(formData: FormData) {
+  const supabase = await createClient()
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+  const { error } = await supabase.auth.signInWithPassword(data)
+  if (error) {
+    redirect('/error')
+  }
+  revalidatePath('/', 'layout')
+  redirect('/')
+}
+
+export async function signup(formData: FormData) {
+  const supabase = await createClient()
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+  const { error } = await supabase.auth.signUp(data)
+  if (error) {
+    redirect('/error')
+  }
+  revalidatePath('/', 'layout')
+  redirect('/')
+}
+
+export async function signout() {
+  const supabase = await createClient();
+  const { error } = await supabase.auth.signOut();
+
+  if (error) {
+    redirect("/error");
+  }
+
+  revalidatePath("/", "layout");
+  redirect("/");
+}
+
+export async function updateNickname(formData: FormData) {
+  const supabase = await createClient();
+  const data = {
+    nickname: formData.get('nickname') as string,
+  }
+
+  const { data: userData } = await supabase.auth.getUser();
+  const userId = userData.user?.id;
+ 
+  const { error } = await supabase
+    .from("profiles")
+    .update({ nickname: data.nickname })
+    .eq("id", userId);
+
+    if (error) {
+      redirect('/error')
+    }
+
+  redirect("/");
+}
