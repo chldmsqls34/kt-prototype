0be2eabd1abd53e8b64cb6ac13fad9318fcbcd7c
chldmsqls34 <chldmsqls34@gmail.com>
2024-12-18 04:53:48 +0900

    feat: 게시글 id기반 상세 데이터 가져오는 함수 추가

--- src/services/post-service.ts
+++ src/services/post-service.ts
@@ -1,4 +1,4 @@
-import { PostListItem } from "@/types";
+import { PostDetail, PostListItem } from "@/types";
 import { createClient } from "@/utils/supabase/server";
 
 export async function fetchPostsPages(
@@ -80,5 +80,75 @@ export async function fetchFilteredPost(
   }
 }
 
+
+export async function fetchPostById(postId: number): Promise<PostDetail | null> {
+  try {
+    const supabase = await createClient();
+    const { data, error } = await supabase
+      .from("posts")
+      .select(`
+        id,
+        user_id,
+        title,
+        content,
+        created_at,
+        view_count,
+        author,
+        images (
+          image_url
+        ),
+        comments (
+          id,
+          post_id,
+          user_id,
+          content,
+          created_at,
+          author
+        )
+      `)
+      .eq("id", postId)
+      .single();
+
+      if (error) {
+        throw new Error(`Error fetching posts: ${error.message}`);
+      }
+      
+      if (!data) {
+        return null;
+      }
+    const clientProjectData: PostDetail = {
+      id: data.id,
+      userId: data.user_id,
+      title: data.title,
+      content: data.content,
+      createdAt: new Date(data.created_at).toLocaleDateString('ko-KR', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      }),
+      viewCount: data.view_count,
+      author: data.author,
+      images: data.images.map((image) => image.image_url),
+      comments: data.comments.map((comment) => ({
+        id: comment.id,
+        postId: comment.post_id,
+        userId: comment.user_id,
+        content: comment.content,
+        createdAt: new Date(comment.created_at).toLocaleDateString('ko-KR', {
+          year: 'numeric',
+          month: '2-digit',
+          day: '2-digit',
+          hour: '2-digit',
+          minute: '2-digit',
+        }),
+        author: comment.author,
+      })),
+    };
+    return clientProjectData;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
 
 
