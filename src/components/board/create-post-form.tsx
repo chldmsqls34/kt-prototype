a2d785980dd8cb60c415428a5d8269d45979aa21
chldmsqls34 <chldmsqls34@gmail.com>
2024-12-24 03:16:59 +0900

    refactor: react-hook-form + zod

--- src/components/board/create-post-form.tsx
+++ src/components/board/create-post-form.tsx
@@ -1,28 +1,60 @@
 "use client";
-import { createPost } from "@/services/post-action";
-import { useFormState } from "react-dom";
+
 import { useState } from "react";
 import { PaperClipIcon } from "@heroicons/react/24/outline";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createPost } from "@/services/post-action";
+
+const postSchema = z.object({
+  title: z.string().min(2, "제목을 2글자 이상 입력하세요").max(100, "제목은 100자 이내여야 합니다."),
+  content: z.string().min(2, "내용을 2글자 이상 입력하세요").max(1000, "내용은 1000자 이내여야 합니다."),
+  images: z
+    .array(
+      z.object({
+        file: z.instanceof(File),
+        preview: z.string(),
+      })
+    )
+    .max(3, "이미지는 최대 3개까지 업로드할 수 있습니다.")
+    .optional(),
+});
+
+type PostFormValues = z.infer<typeof postSchema>;
 
 export default function CreatePostForm() {
   const router = useRouter();
-  const initialState = { message: '', errors: {} };
-  const [state, formAction] = useFormState(createPost, initialState);
-  const [images, setImages] = useState<{ file: File; preview: string }[]>([]);
+  const [imagePreviews, setImagePreviews] = useState<{ file: File; preview: string }[]>([]);
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+  } = useForm<PostFormValues>({
+    resolver: zodResolver(postSchema),
+    defaultValues: {
+      title: "",
+      content: "",
+      images: [],
+    },
+  });
 
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-
     const files = e.target.files ? Array.from(e.target.files) : [];
+    const existingImages = watch("images") || [];
     // 중복된 파일 제거 (파일명과 수정시간이 일치하는 파일 제외)
     const newFiles = files.filter(
       (file) =>
-        !images.some((image) => image.file.name === file.name && image.file.lastModified === file.lastModified)
+        !existingImages.some((image) => image.file.name === file.name && image.file.lastModified === file.lastModified)
     );
 
     // 업로드 제한 (최대 3개)
-    if (newFiles.length + images.length > 3) {
+    if (newFiles.length + existingImages.length > 3) {
       alert("이미지는 최대 3개까지 업로드할 수 있습니다.");
       return;
     }
@@ -31,57 +63,52 @@ export default function CreatePostForm() {
       preview: URL.createObjectURL(file),
     }));
 
-    setImages((prev) => [...prev, ...newImagePreviews]);
+    setImagePreviews((prev) => [...prev, ...newImagePreviews]);
+    setValue("images", [...existingImages, ...newImagePreviews]);
   };
 
   const handleImageRemove = (index: number) => {
-    setImages((prev) => prev.filter((_, i) => i !== index));
+    const updatedImages = watch("images")?.filter((_, i) => i !== index) || [];
+    setImagePreviews(updatedImages);
+    setValue("images", updatedImages);
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const formData = new FormData(e.currentTarget);
-    images.forEach((img) => formData.append("images", img.file));
-
-    formAction(formData);
-    setImages([]);
+  const onSubmit = async(data: PostFormValues) => {
+    const formData = new FormData();
+    formData.append("title", data.title);
+    formData.append("content", data.content);
+    data.images?.forEach((img) => formData.append("images", img.file));
+    await createPost(formData);
+    setImagePreviews([]);
   };
 
   return (
     <div className="flex items-center justify-center">
       <div className="w-full py-10">
-        <form onSubmit={handleSubmit} className="relative">
+        <form onSubmit={handleSubmit(onSubmit)} className="relative">
           <div className="rounded-lg bg-white outline outline-1 -outline-offset-1 outline-gray-300">
             <label htmlFor="title" className="sr-only">
               Title
             </label>
-            <input
-              id="title"
-              name="title"
-              type="text"
-              placeholder="제목"
-              className="block w-full p-5 font-medium text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 border-b"
-              required
-            />
-            {state.errors?.title && (
-              <p className="text-[--main-red-color] text-sm">{state.errors.title[0]}</p>
-            )}
+            <div className="flex whitespace-nowrap border-b">
+              <input
+                type="text"
+                placeholder="제목"
+                className="block w-full p-5 font-medium text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0"
+                {...register('title')}
+              />
+              {errors.title && <p className="text-[--main-red-color] text-xs p-5">{errors.title.message}</p>}
+            </div>
             <label htmlFor="content" className="sr-only">
               Content
             </label>
             <textarea
-              id="content"
-              name="content"
               rows={10}
               placeholder="내용을 입력하세요"
               className="block w-full resize-none p-5 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
-              defaultValue={''}
-              required
+              {...register('content')}
             />
-            {state.errors?.content && (
-              <p className="text-[--main-red-color] text-sm">{state.errors.content[0]}</p>
-            )}
+            {errors.content && <p className="text-[--main-red-color] text-xs p-5">{errors.content.message}</p>}
           </div>
 
           <div className="px-3 py-3">
@@ -93,11 +120,8 @@ export default function CreatePostForm() {
               onChange={handleImageChange}
               className="hidden"
             />
-            {state.errors?.imageFiles && (
-              <p className="text-[--main-red-color] text-sm mt-1">{state.errors.imageFiles[0]}</p>
-            )}
             <div className="flex gap-4 mt-3 min-h-[80px]">
-              {images.map((img, index) => (
+              {imagePreviews.map((img, index) => (
                 <div key={index} className="relative">
                   <Image
                     src={img.preview}
@@ -117,11 +141,8 @@ export default function CreatePostForm() {
                 </div>
               ))}
             </div>
+            {errors.images && <p className="text-[--main-red-color] text-sm">{errors.images.message}</p>}
           </div>
-          {state.message && (
-            <div className="text-center text-[--main-red-color] text-sm">{state.message}</div>
-          )}
-
           <div className="flex justify-between items-center mt-5 border-t border-gray-200 pt-3">
             <label htmlFor="file-input" className="flex items-center text-gray-500 hover:text-gray-600 cursor-pointer">
               <PaperClipIcon className="h-5 w-5 mr-2" aria-hidden="true" />
--- src/services/post-action.ts
+++ src/services/post-action.ts
@@ -6,41 +6,32 @@ import { redirect } from "next/navigation";
 import { z } from "zod";
 import { v4 as uuidv4 } from 'uuid';
 
-const CreateFormSchema = z.object({
-  title: z.string().min(2, { message: '제목을 2글자 이상 입력해주세요' }),
-  content: z.string().min(2, { message: '내용을 2글자 이상 입력해주세요' }),
-  imageFiles: z.array(z.instanceof(File))
-    .optional()
-    .refine(
-      (files) => !files || files.every((file) => file.type.includes('image')),
-      { message: '이미지 파일만 첨부할 수 있습니다' }
+const postSchema = z.object({
+  title: z.string().min(2, "제목을 2글자 이상 입력하세요").max(100, "제목은 100자 이내여야 합니다."),
+  content: z.string().min(2, "내용을 2글자 이상 입력하세요").max(1000, "내용은 1000자 이내여야 합니다."),
+  images: z
+    .array(
+      z.object({
+        file: z.instanceof(File).optional(),
+        preview: z.string(),
+      })
     )
-    .refine(
-      (files) => !files || files.length <= 3,
-      { message: '이미지는 최대 3개까지 첨부할 수 있습니다' }
-    ),
+    .max(3, "이미지는 최대 3개까지 업로드할 수 있습니다.")
+    .optional(),
 });
 
-export interface PostState{
-  errors?:{
-    title?: string[];
-    content?: string[];
-    imageFiles?: string[];
-  };
-  message?: string | null;
-};
 
-export async function createPost(prevState:PostState, formData: FormData) {
+export async function createPost(formData: FormData) {
   try {
     const supabase = await createClient();
-    const imageFiles = formData
+    const images = formData
       .getAll('images')
       .filter((file): file is File => file instanceof File);
 
-    const validateFields = CreateFormSchema.safeParse({
+    const validateFields = postSchema.safeParse({
       title: formData.get('title') as string,
       content: formData.get('content') as string,
-      imageFiles: imageFiles.length > 0 ? imageFiles : undefined,
+      images: images.length > 0 ? images : undefined,
     });
   
     if(!validateFields.success){
@@ -87,9 +78,9 @@ export async function createPost(prevState:PostState, formData: FormData) {
     }
     const postId = postData.id;
     
-    if (imageFiles.length) {
+    if (images.length) {
       const uploadResults = await Promise.allSettled(
-        imageFiles.map(async (image) => {
+        images.map(async (image) => {
           const extension = image.name.split('.').pop() || 'png';
           const fileName = `${Date.now()}_${uuidv4()}.${extension}`;
 
@@ -128,16 +119,16 @@ export async function createPost(prevState:PostState, formData: FormData) {
 export async function updatePost(formData: FormData, postId: number) {
   try {
     const supabase = await createClient();
-    const imageFiles = formData
+    const images = formData
       .getAll('images')
       .filter((file): file is File => file instanceof File);
 
     const existingImages = formData.getAll('existingImages') as string[];
 
-    const validateFields = CreateFormSchema.safeParse({
+    const validateFields = postSchema.safeParse({
       title: formData.get('title') as string,
       content: formData.get('content') as string,
-      imageFiles: imageFiles.length > 0 ? imageFiles : undefined,
+      images: images.length > 0 ? images : undefined,
     });
   
     if (!validateFields.success) {
@@ -201,9 +192,9 @@ export async function updatePost(formData: FormData, postId: number) {
     }
 
     // 새 이미지 업로드
-    if (imageFiles.length > 0) {
+    if (images.length > 0) {
       const uploadResults = await Promise.allSettled(
-        imageFiles.map(async (image) => {
+        images.map(async (image) => {
           const extension = image.name.split('.').pop() || 'png';
           const fileName = `${Date.now()}_${uuidv4()}.${extension}`;
 
