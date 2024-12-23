4df4675a0b53e4437ac585a540f4d2938301ad9c
chldmsqls34 <chldmsqls34@gmail.com>
2024-12-23 18:24:07 +0900

    feat: 댓글 작성

--- src/components/board/post-detail-box.tsx
+++ src/components/board/post-detail-box.tsx
@@ -1,9 +1,10 @@
 "use client";
 import { PostDetail } from "@/types";
 import { UserIcon } from "@heroicons/react/20/solid";
-import { CalendarDaysIcon, CheckCircleIcon } from "@heroicons/react/24/outline";
+import { CalendarDaysIcon, CheckCircleIcon, PlusIcon } from "@heroicons/react/24/outline";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
+import { createComment } from "@/services/comment-action";
 
 interface PostDetailBoxProps {
   post: PostDetail;
@@ -11,6 +12,15 @@ interface PostDetailBoxProps {
 
 export default function PostDetailBox({post}: PostDetailBoxProps) {
   const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+    const postId = post.id;
+    await createComment(formData, postId);
+    form.reset();
+  };
 
   return (
     <div className="flex items-center justify-center">
@@ -56,8 +66,44 @@ export default function PostDetailBox({post}: PostDetailBoxProps) {
             목록보기
           </button>
         </div>
-
+        <div className="space-y-4 pt-8">
+          <h1>댓글</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="flex space-x-2">
+              <textarea
+                id="content"
+                name="content"
+                rows={3}
+                placeholder="내용을 입력하세요"
+                className="block w-full resize-none p-5 text-sm border-2"
+                required
+              />
+              <button
+                type="submit"
+                className="bg-gray-600 text-white text-sm px-6 py-2 rounded hover:bg-gray-500"
+              >
+                <PlusIcon className="w-5 h-5" />
+              </button>
+            </div>
+          </form>
+          <div className="border-b py-4">
+            {post.comments?.map((comment, index) => (
+              <div key={index} className="flex w-full">
+                <div className="flex flex-col text-sm w-full border-t">
+                  <div className="text-gray-800 w-full p-4 break-words space-y-8">
+                    <div className="whitespace-pre-wrap">{comment.content}</div>
+                    <div className="flex justify-between items-center mb-1">
+                      <div className="text-gray-600 font-semibold text-xs">{comment.author}</div>
+                      <div className="text-gray-400 text-xs">{comment.createdAt}</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
+
     </div>
 
   );
